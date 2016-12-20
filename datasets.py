d7440f1cbdfb056290ce7067b33cda390a1fdfe3
Leslie_Yan <leslieyan0621@gmail.com>
2016-12-20 19:39:57 +0800

    Read data

--- datasets.py
+++ datasets.py
@@ -5,13 +5,12 @@ import pandas as pd
 
 class ReadData(object):
 
-    def __init__(self, filename, key):
-        self.filename = filename
-        self.key = key
+    def __init__(self):
         self.dataframe = None
 
     def run(self):
-        with pd.HDFStore("train.h5", "r") as train:
-            self.dataframe = train.get('train')
-
-
+        self.dataframe = pd.read_table(filepath_or_buffer='train.csv',
+                                       sep=',',
+                                       header=0,
+                                       index_col=0)
+        return self.dataframe
