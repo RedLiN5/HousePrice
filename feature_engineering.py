2418a663283ad1d290ce9679955795147f6ed4bd
Leslie_Yan <leslieyan0621@gmail.com>
2016-12-20 22:57:41 +0800

    Add function docstrings

--- feature_engineering.py
+++ feature_engineering.py
@@ -12,11 +12,20 @@ class FeatureEngin(ReadData):
         self.colnum = self.dataframe.shape[1]
 
     def _missing_value(self):
+        """
+        Count number of missing values in each column.
+        :return:
+        Series
+        """
         dataframe = self.dataframe
         missing_count = dataframe.isnull().sum()
         return missing_count
 
     def _remove_missing(self):
+        """
+        Remove columns with more than 40% missing values.
+        :return:
+        """
         missing_count = self._missing_value()
         col_remove = missing_count[missing_count>self.colnum * .4]
         colname_remove = col_remove.index.tolist()
@@ -24,7 +33,10 @@ class FeatureEngin(ReadData):
                                              axis = 1)
 
     def _convert(self):
+        """
+        Convert string to int in each column.
+        :return:
+        """
         self._remove_missing()
         colname = self.dataframe.columns
         value_count = [] * self.dataframe.shape[0]
-        
