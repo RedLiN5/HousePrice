58d46c7be0e065c82c4923b8ec9a51511d3adbfc
Leslie_Yan <leslieyan0621@gmail.com>
2016-12-25 18:08:16 +0800

    [Feature] Added feature selection

--- feature_engineering.py
+++ feature_engineering.py
@@ -5,7 +5,6 @@ import numpy as np
 from feature_preprocessing import FeaturePreprocess
 import minepy
 from scipy.stats import pearsonr
-import matplotlib.pyplot as plt
 
 class FeatureEngin(FeaturePreprocess):
 
@@ -34,3 +33,16 @@ class FeatureEngin(FeaturePreprocess):
         corr_scores = corr_scores.sort_values(ascending=False)
         return corr_scores
 
+    def _feature_select_(self):
+        corr_scores = self._feature_impact_()
+        score_ratios = corr_scores / sum(corr_scores)
+        for i in range(len(score_ratios)):
+            while sum(score_ratios) >= .8:
+                break
+
+        feature_names = corr_scores.index.tolist()[:i]
+        return feature_names
+
+    def _collinearity_handle_(self):
+        # TODO(Leslie) Handle collinearity in features
+        pass
