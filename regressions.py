007e96dbb9bd46d3cfac629b01e471eb562b637f
Leslie_Yan <leslieyan0621@gmail.com>
2016-12-29 13:46:34 +0800

    [Feature] eXtreme Gradient Boosting Regressor

--- regressions.py
+++ regressions.py
@@ -2,9 +2,11 @@
 
 from feature_engineering import FeatureEngin
 from sklearn.linear_model import Ridge
+from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
+from sklearn.metrics import r2_score
 
 class Regressions(FeatureEngin):
 
@@ -44,13 +46,54 @@ class Regressions(FeatureEngin):
                     solver='auto',
                     random_state=1,
                     tol=.001)
-        clf.fit(self.X.copy(), self.y.copy())
+        clf.fit(self.X.copy(),
+                self.y.copy())
         preds = clf.predict(X=X_test)
         ids = X_test.index
         pred_df = pd.DataFrame({'SalePrice':preds},
                                index=ids)
         pred_df.to_csv('results_ridge.csv',
                        sep=',')
+
+    def xgb_reg(self):
+        X_train, X_test, y_train, y_test = self._split_data_()
+        gammas = [0.1, 0.5, 1, 2, 5, 8, 10]
+        scores = [None] * len(gammas)
+        i = 0
+        for gamma in gammas:
+            clf = XGBRegressor(max_depth=7,
+                               learning_rate=0.11,
+                               n_estimators=300,
+                               gamma=1,
+                               seed=1)
+            clf.fit(X_train.values,
+                    y_train.values.flatten())
+            scores[i] = clf.score(X_test.values,
+                                  y_test.values.flatten())
+            i = i + 1
+
+        print(gammas)
+        print(scores)
+        depth = 7
+        learning_rate = 0.11
+        n_estimator = 300
+        gamma = 1
+
+    def fit_xgb(self, X_test):
+        clf = XGBRegressor(max_depth=7,
+                           learning_rate=0.11,
+                           n_estimators=300,
+                           gamma=1,
+                           seed=1)
+        clf.fit(self.X.copy().values,
+                self.y.copy().values.flatten())
+        preds = clf.predict(data=X_test.values)
+        ids = X_test.index
+        pred_df = pd.DataFrame({'SalePrice': preds},
+                               index=ids)
+        pred_df.to_csv('results_xgb.csv',
+                       sep=',')
+
 
     def run(self):
         # TODO(Leslie) add last function to run
