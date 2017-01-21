24f2f3a6e4686cf121630fecb40928a97105c973
Leslie_Yan <leslieyan0621@gmail.com>
2017-01-22 00:24:47 +0800

    [Improve] Plot style

--- feature_effect_plots.py
+++ feature_effect_plots.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 from feature_preprocessing import FeaturePreprocess
 import pandas as pd
 import numpy as np
@@ -16,5 +17,14 @@ figure, axes = plt.subplots(n, 1)
 for i, feature in enumerate(features_name):
     axes[i].scatter(df[feature], y,
                     facecolor = 'c')
+    axes[i].set_xlabel(feature)
+    axes[i].set_yticks(np.linspace(-1*10**5, 9*10**5, 6))
+    axes[i].yaxis.set_major_formatter(
+        mtick.FormatStrFormatter('%.2e'))
+
+figure.text(0, 0.5, 'SalePrice', va='center', rotation='vertical')
+plt.subplots_adjust(left=0.15, bottom=0.1,
+                    right=0.9, top=0.9,
+                    wspace=None, hspace=1)
 
 plt.savefig('feature_effect_plots.png')
