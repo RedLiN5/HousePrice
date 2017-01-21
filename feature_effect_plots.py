0b4225b26f89eb5f33e5351f03ba3e01099fc01c
Leslie_Yan <leslieyan0621@gmail.com>
2017-01-21 23:57:23 +0800

    [Improve] Loop

--- feature_effect_plots.py
+++ feature_effect_plots.py
@@ -9,14 +9,12 @@ df = reader.run_preprocessor()
 print(df.columns)
 y = np.array(df['SalePrice'])
 
-OverallQual = df['OverallQual'].values.flatten()
-GrLivArea = df['GrLivArea']
-GarageCars = df['GarageCars']
-GarageArea = df['GarageArea']
+features_name = ['OverallQual', 'GrLivArea', 'GarageCars', 'GarageArea']
+n = len(features_name)
+figure, axes = plt.subplots(n, 1)
 
-figure, axes = plt.subplots(4, 1)
-axes[0].scatter(OverallQual, y)
-axes[1].scatter(GrLivArea, y)
-axes[2].scatter(GarageCars, y)
-axes[3].scatter(GarageArea, y)
+for i, feature in enumerate(features_name):
+    axes[i].scatter(df[feature], y,
+                    facecolor = 'c')
+
 plt.savefig('feature_effect_plots.png')
