0dd49bbbad446e8401eb3955f6ba3679b9bc3330
Leslie_Yan <leslieyan0621@gmail.com>
2017-01-22 17:57:50 +0800

    [Improve] Adjusted size and color

--- feature_effect_plots.py
+++ feature_effect_plots.py
@@ -4,27 +4,35 @@ from feature_preprocessing import FeaturePreprocess
 import pandas as pd
 import numpy as np
 
-reader = FeaturePreprocess('train.csv', ispred=True)
+reader = FeaturePreprocess('train.csv', ispred=False)
 df = reader.run_preprocessor()
 
-print(df.columns)
+# print(len(df.columns))
 y = np.array(df['SalePrice'])
 
-features_name = ['OverallQual', 'GrLivArea', 'GarageCars', 'GarageArea']
+features_name = ['OverallQual', 'GrLivArea', 'GarageCars', 'GarageArea',
+                 'TotalBsmtSF', '1stFlrSF', 'FullBath', 'TotRmsAbvGrd',
+                 'BuiltAge', 'RemodelAge', 'MasVnrArea', 'Fireplaces']
+
+# features_name = df.columns[61:]
 n = len(features_name)
 figure, axes = plt.subplots(n, 1)
+color = (0.5529411764705883, 0.6274509803921569, 0.796078431372549)
+
+figure.set_size_inches(10, 50)
 
 for i, feature in enumerate(features_name):
     axes[i].scatter(df[feature], y,
-                    facecolor = 'c')
+                    facecolor = color,
+                    s = 15)
     axes[i].set_xlabel(feature)
     axes[i].set_yticks(np.linspace(-1*10**5, 9*10**5, 6))
     axes[i].yaxis.set_major_formatter(
-        mtick.FormatStrFormatter('%.2e'))
+        mtick.FormatStrFormatter('%.1e'))
 
-figure.text(0, 0.5, 'SalePrice', va='center', rotation='vertical')
-plt.subplots_adjust(left=0.15, bottom=0.1,
-                    right=0.9, top=0.9,
-                    wspace=None, hspace=1)
+figure.text(0.01, 0.5, 'SalePrice', va='center', rotation='vertical')
+plt.subplots_adjust(left=0.15, bottom=0.01,
+                    right=0.9, top=0.99,
+                    wspace=None, hspace=0.5)
 
 plt.savefig('feature_effect_plots.png')
