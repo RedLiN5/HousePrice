fd0e0b8e4c542f66ed980514e30dc38e3a44e4a5
Leslie_Yan <leslieyan0621@gmail.com>
2017-01-22 17:54:31 +0800

    [Improve] Outliers, feature encoding

--- feature_preprocessing.py
+++ feature_preprocessing.py
@@ -39,7 +39,24 @@ class FeaturePreprocess(ReadData):
         self._remove_missing_()
         df = self.dataframe
         df.drop(df[df["GrLivArea"] > 4000].index,
+                inplace = True)
+        df.drop(df[df['LotFrontage'] > 160].index,
+                inplace = True)
+        df.drop(df[df['LotArea'] > 60000].index,
                 inplace=True)
+        df.drop(df[df['MasVnrArea'] > 1250].index,
+                inplace=True)
+        df.drop(df[df['TotalBsmtSF'] > 2800].index,
+                inplace=True)
+        df.drop(df[df['1stFlrSF'] > 2500].index,
+                inplace=True)
+        df.drop(df[df['GarageArea'] > 1200].index,
+                inplace=True)
+        df.drop(df[df['WoodDeckSF'] > 600].index,
+                inplace=True)
+        df.drop(df[df['OpenPorchSF'] > 350].index,
+                inplace=True)
+
         self.dataframe = df
 
 
@@ -50,8 +67,7 @@ class FeaturePreprocess(ReadData):
         """
         self._domain_knwl_encod()
         dataframe = self.dataframe.copy()
-        colnames = self.dataframe.columns
-        value_count = [] * self.dataframe.shape[0]
+        colnames = dataframe.columns
 
         for name in colnames:
             column = dataframe[name]
@@ -91,6 +107,33 @@ class FeaturePreprocess(ReadData):
         if not self.ispred:
             self._remove_outliers()
         df = self.dataframe
+        df["RegularLotShape"] = (df['LotShape'] == 4) * 1
+        df["IsLandLevel"] = (df['LandContour'] == "Lvl") * 1
+        df['AllUtilities'] = (df['Utilities'] == 'AllPub') * 1
+        df['GentleLandSlope'] = (df['LandSlope'] == 3) * 1
+        df["IsElectricalSBrkr"] = (df['Electrical'] == "SBrkr") * 1
+        df["Has2ndFloor"] = (df['2ndFlrSF'] == 0) * 1
+        df["IsLowQual"] = (df['LowQualFinSF'] == 0) * 1
+        df["IsGarageDetached"] = (df['GarageType'] == 1) * 1
+        df["VeryNewHouse"] = (df['YearBuilt'] == df["YrSold"]) * 1
+        df['BuiltAge'] = 2011 - df['YearBuilt']
+        df['RemodelAge'] = 2011 - df['YearRemodAdd']
+        # df.drop(['YearBuilt', 'YearRemodAdd', 'LotShape', 'LandContour',
+        #                 'Utilities', 'LandSlope', 'Electrical', 'GarageType'],
+        #                axis = 1,inplace = True)
+        df["HighSeason"] = df["MoSold"].replace(
+            {1: 0, 2: 0, 3: 0, 4: 1, 5: 1, 6: 1, 7: 1, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0})
+        df["MSSubClass"] = df["MSSubClass"].replace(
+            {20: 1, 30: 0, 40: 0, 45: 0, 50: 0, 60: 1, 70: 0, 75: 0, 80: 0, 85: 0,
+             90: 0, 120: 1, 150: 0, 160: 0, 180: 0, 190: 0})
+        df.loc[df.Neighborhood == 'NridgHt', "Neighborhood_Good"] = 1
+        df.loc[df.Neighborhood == 'Crawfor', "Neighborhood_Good"] = 1
+        df.loc[df.Neighborhood == 'StoneBr', "Neighborhood_Good"] = 1
+        df.loc[df.Neighborhood == 'Somerst', "Neighborhood_Good"] = 1
+        df.loc[df.Neighborhood == 'NoRidge', "Neighborhood_Good"] = 1
+        df["Neighborhood_Good"].fillna(0, inplace=True)
+        df.drop(['MoSold'], axis=1, inplace=True)
+
         qual_dict = {'NA': 0, "Po": 1, "Fa": 2, "TA": 3, "Gd": 4, "Ex": 5}
         df['LotShape'] = df['LotShape'].map({'Reg': 4, 'IR1': 3,
                                              'IR2': 2, 'IR3': 1})
@@ -128,14 +171,28 @@ class FeaturePreprocess(ReadData):
         df["Functional"] = df["Functional"].map({None: 0, "Sal": 1, "Sev": 2,
                                                  "Maj2": 3, "Maj1": 4, "Mod": 5,
                                                  "Min2": 6, "Min1": 7, "Typ": 8})
-        df['SaleCondition'] = df['SaleCondition'].map({'Abnorml': 1, 'Alloca': 1,
-                                                       'AdjLand': 1, 'Family': 1, 'Normal': 0,
-                                                       'Partial': 0})
-        df['BuiltAge'] = 2011 - df['YearBuilt']
-        df['RemodelAge'] = 2011 - df['YearRemodAdd']
-        df.drop(['YearBuilt', 'YearRemodAdd'],
-                axis = 1,
-                inplace = True)
+        df['SaleCondition_PriceDown'] = df['SaleCondition'].map({'Abnorml': 1, 'Alloca': 1,
+                                                                 'AdjLand': 1, 'Family': 1,
+                                                                 'Normal': 0, 'Partial': 0})
+        df["BoughtOffPlan"] = df['SaleCondition'].map({"Abnorml": 0, "Alloca": 0, "AdjLand": 0,
+                                                       "Family": 0, "Normal": 0, "Partial": 1})
+
+        df["SimplOverallQual"] = df['OverallQual'].map({1: 1, 2: 1, 3: 1, 4: 2, 5: 2,
+                                                        6: 2, 7: 3, 8: 3, 9: 3, 10: 3})
+        df["SimplOverallCond"] = df['OverallCond'].map({1: 1, 2: 1, 3: 1, 4: 2, 5: 2,
+                                                        6: 2, 7: 3, 8: 3, 9: 3, 10: 3})
+        df["SimplGarageCond"] = df['GarageCond'].map({1: 1, 2: 1, 3: 1, 4: 2, 5: 2})
+        df["SimplGarageQual"] = df['GarageQual'].map({1: 1, 2: 1, 3: 1, 4: 2, 5: 2})
+        df["SimplFunctional"] = df['Functional'].map({1: 1, 2: 1, 3: 2, 4: 2, 5: 3, 6: 3, 7: 3, 8: 4})
+        df["SimplKitchenQual"] = df['KitchenQual'].map({1: 1, 2: 1, 3: 1, 4: 2, 5: 2})
+        df["SimplHeatingQC"] = df['HeatingQC'].map({1: 1, 2: 1, 3: 1, 4: 2, 5: 2})
+        df["SimplBsmtFinType1"] = df['BsmtFinType1'].map({1: 1, 2: 1, 3: 1, 4: 2, 5: 2, 6: 2})
+        df["SimplBsmtFinType2"] = df['BsmtFinType2'].map({1: 1, 2: 1, 3: 1, 4: 2, 5: 2, 6: 2})
+        df["SimplBsmtCond"] = df['BsmtCond'].map({1: 1, 2: 1, 3: 1, 4: 2, 5: 2})
+        df["SimplBsmtQual"] = df['BsmtQual'].map({1: 1, 2: 1, 3: 1, 4: 2, 5: 2})
+        df["SimplExterCond"] = df['ExterCond'].map({1: 1, 2: 1, 3: 1, 4: 2, 5: 2})
+        df["SimplExterQual"] = df['ExterQual'].map({1: 1, 2: 1, 3: 1, 4: 2, 5: 2})
+
         self.dataframe = df
 
     def run_preprocessor(self):
