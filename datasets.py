d226f4835bbc193aaf40d48fffbe1ccbdc3e32c6
Leslie_Yan <leslieyan0621@gmail.com>
2017-05-08 12:46:37 +0800

    [Debug] Change class structure

--- datasets.py
+++ datasets.py
@@ -5,13 +5,21 @@ import pandas as pd
 
 class ReadData(object):
 
-    def __init__(self, filname):
-        self.dataframe = None
-        self.filename = filname
+    def __init__(self, trainFile, testFile):
+        trainData = pd.read_table(filepath_or_buffer=trainFile,
+                                  sep=',',
+                                  header=0,
+                                  index_col=0)
+        testData = pd.read_table(filepath_or_buffer=testFile,
+                                 sep=',',
+                                 header=0,
+                                 index_col=0)
+        y = trainData['SalePrice']
+        trainData.drop('SalePrice',
+                       axis=1,
+                       inplace=True)
+        self.dataframe = pd.concat([trainData, testData])
+        self.trainIndex = self.dataframe.index[:1460]
+        self.testIndex = self.dataframe.index[1460:]
 
-    def load(self):
-        self.dataframe = pd.read_table(filepath_or_buffer=self.filename,
-                                       sep=',',
-                                       header=0,
-                                       index_col=0)
-        return self.dataframe
+
